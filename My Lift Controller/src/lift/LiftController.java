d9bd0bb191d8846a96ca14aa2a4826e57c39fc3e
2stef22 <evripidou.stefanos2@gmail.com>
2018-01-14 16:29:32 +0000

    Updated and probably final version

--- My Lift Controller/src/lift/LiftController.java
+++ My Lift Controller/src/lift/LiftController.java
@@ -2,78 +2,124 @@
 package lift;
 
 /**
- * Interface that lift controllers need to provide.
- * @author Kevin Bryson
+ * Null implementation of a Lift Controller.
+ * @author K. Bryson
  */
-public interface LiftController {
+public class MyLiftController implements LiftController {
 
-    /********************/
-    /* Global constants */
-    /********************/
+	int currentfloor = 0; //holds the current floor
+	int[] endfloor = {0,0,0,0,0,0,0,0,0}; //store all the floor destinations. These arrays are of size 9 to represent floors 0 to 8.
+	int[] startupfloor = {0,0,0,0,0,0,0,0,0}; //to store  the pushUpButton requests
+	int[] startdownfloor = {0,0,0,0,0,0,0,0,0}; //to store the pushDown requests
+	volatile boolean doorsOpen  = false; // to check if the doors are open
+	Direction currentDirection  = Direction.UP; //holds the direction the lift is going
+	
+	
+	
+    /* Interface for People */
+    public synchronized void pushUpButton(int floor) throws InterruptedException {
+ 
+    	notifyAll();
+    	startupfloor[floor] = startupfloor[floor] + 1;  //Store 1 to the correct array position that corresponds to a floor.
     
-    public enum Direction {
-        UP,
-        DOWN,
-        UNSET
+    	 /*This is the same as having (!( (currentDirection == Direction.UP) && (doorsOpen) && (currentfloor == floor))). (De Morgan's Law)
+    	 I just find it easier to understand with the OR statments.
+    	 The person thread will wait until the lift is going up, a sign has been given to open the doors and the floor is correct. */
+    	while( (currentDirection != Direction.UP) || (!doorsOpen) || (currentfloor != floor)) wait(); 
+    
+    	startupfloor[floor] = startupfloor[floor] -1;
+    	//Shows that one person is going to enter the thread and has lift on his floor. 
+  
+    	notifyAll();
+    
+    
+
     }
 
-    /************************/
-    /* Interface for People */
-    /************************/
-
-    /*
-     * Person presses the "Up Button" or "Down Button" on a particular floor.
-     *
-     * NOTE - THESE METHODS DOES NOT RETURN UNTIL THE LIFT HAS APPEARED,
-     * OPENED ITS DOORS, AND THE PERSON HAS 'ENTERED' THE LIFT.
-     *
-     * @param floor        Floor number that the person is on.
-     */
-    void pushUpButton(int floor) throws InterruptedException;
-    void pushDownButton(int floor) throws InterruptedException;
+    public synchronized void pushDownButton(int floor) throws InterruptedException {
+    	
+    	
+    	notifyAll();
+    	/*Same as above but with different array representing pushDown requests as no way to differentiate between Up and Down requests 
+    	 in a single array */
+    	startdownfloor[floor] = startdownfloor[floor] + 1;
+    
+    	
+    	//Again the person waits until doors are open on his floor and with the correct destination.
+    	while((currentDirection != Direction.DOWN) || (!doorsOpen) || (currentfloor !=floor)) wait();
+    
+    	startdownfloor[floor] = startdownfloor[floor]  - 1 ;
+    
+    	notifyAll();
     
     
-    /*
-     * A person is in the lift and selects a particular floor.
-     *
-     * NOTE - THIS METHOD DOES NOT RETURN UNTIL THE LIFT HAS OPENED
-     * IT'S DOOR AT THE SELECTED FLOOR AND THE PERSON HAS 'EXITED'.
-     *
-     * @param floor Floor number that they have selected.
-     */
-    void selectFloor(int floor) throws InterruptedException;
+    }
+    
+    public synchronized void selectFloor(int floor) throws InterruptedException {
+    	
+    	
+    	notifyAll();
+    	
+    	//Only one array this time, as we assume that a person will enter the lift when it is moving with his desired direction.
+    	endfloor[floor] = endfloor[floor] + 1; 
+    
+    	while((currentfloor !=floor) || (!doorsOpen)) wait(); //wait until doors open and correct floor.
 
+    	endfloor[floor] = endfloor[floor] -1 ; //remove one from the list, representing one person thread exiting the method.
+    	
+    	notifyAll();
+    	
+    	
+    }
 
-    /***********************/
+    
     /* Interface for Lifts */
-    /***********************/
+    public synchronized boolean liftAtFloor(int floor, Direction direction) {
+    	
+    	currentfloor = floor;
+    	currentDirection = direction;
+    	
+    	//if all of the arrays have 0 in that floor that means there are no requests for people to enter/exit.
+    	if ((endfloor[floor] == 0) && (startupfloor[floor] == 0 ) && (startdownfloor[floor] == 0 )) 
+    	{
+    		return false;
+    	}
+    	else if  ( (startupfloor[floor] != 0 ) && (direction == Direction.DOWN)) // OR if there is an up request but the elevator is going down
+    	{
+    		return false;
+    	}
+    	else if ((startdownfloor[floor] != 0 ) && (direction == Direction.UP)) // and vise versa.
+    	{
+    		return false;
+    	}
+    	
+    	return true; // all other occassions would mean there is a request and direction is correct.
+    	
+    	
+    }
 
-    /*
-     * Lift informs the lift controller that it is on a
-     * specific floor and going in a particular direction.
-     * The lift controller tells the lift whether it should open its doors.
-     *
-     * @param floor Floor number that the lift is at.
-     * @param direction Direction lift is going in.
-     * @return    True if the lift should open its doors.
-     */
-    boolean liftAtFloor(int floor, Direction direction);
+    public synchronized void doorsOpen(int floor) throws InterruptedException {
+    	
+    	doorsOpen = true; // notify that doors are open so people can exit and then enter.
+    	notifyAll();
+    	
+    	
+    	while(endfloor[floor] > 0 ) wait(); //wait until all people that have reached their floor 
+    	 
+    	/*now that everyone has exited allow people in. 
+    	Need to check direction again otherwise people who maybe going the other direction will enter as well.  */
+    	
+    	if(currentDirection ==Direction.UP) while(startupfloor[floor] > 0 ) wait();  
+    		 
+        else if(currentDirection == Direction.DOWN) while(startdownfloor[floor] > 0 ) wait();
+    	
+    	notifyAll();
+    
+    }
 
-    /*
-     * Lift informs the controller that it has its doors open.
-     *
-     * NOTE - the controller should allow all people to
-     * exit and enter the lift before returning.
-     *
-     * @param floor  Floor number that the lift is at.
-     */
-    void doorsOpen(int floor) throws InterruptedException;
-
-    /*
-     * Lift informs the controller that it has now closed its doors.
-     *
-     * @param floor  Floor number that the lift is at.
-     */
-    void doorsClosed(int floor);
-
+    public synchronized void doorsClosed(int floor) {
+    	//If we have reached this method that means that all people have successfully exited and entered the lift. Need to notify that doors are closing
+    	doorsOpen = false;
+    	notifyAll();
+    }
 }
